1b3d21c44f3ad091d98f669a27574ed9e3a4f107
Mintra Phophaijit <bluembc@gmail.com>
2019-12-10 20:47:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -11,7 +11,10 @@ def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
- 		{
+ 		programmer_hash = { :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+          :language => ["C"]
 
 def dennis_ritchies_language
 	programmer_hash = 
