78f2e305259f1d5537c11d65b354a62d18894e94
Mintra Phophaijit <bluembc@gmail.com>
2019-12-10 20:49:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,19 +19,11 @@ def alan_kay_is_known_for
 def dennis_ritchies_language
 	programmer_hash = 
  		{
-        :grace_hopper => {
+         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+          :language => ["C", "LISP"]
+         }
 
 dennis_ritchies_language => {:languages => "C"
 }
